7867455ef65ca667e6c646e14080a1c2ec1a10f7
macastrogimenez <migca@itu.dk>
2025-07-30 16:16:03 +0200

    One exercise done and a few of practice on the practice project

--- Exercise4/Program.fs
+++ Exercise4/Program.fs
@@ -1,2 +1,19 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿(*
+Exercise 4.1 Write a function
+explode:string->char list
+so that explode s returns the list of characters in s:
+explode "star" = [’s’;’t’;’a’;’r’]
+Hint: if s is a string then s.ToCharArray() returns an array of characters. You can then use List.ofArray
+to turn it into a list of characters.
+Now write a function
+explode2:string->char list
+similar to explode except that you now have to use the string function s.Chars (or .[]), where s is a string.
+You can also make use of s.Remove(0,1). The definition of explode2 will be recursive
+*)
+
+let explode (s:string) = s.ToCharArray() |> List.ofArray
+
+let rec explode2 (s:string) = 
+    match s with
+    |"" -> []
+    |_ -> s.[0]:: explode2 (s.Remove(0,1))
---
+++ PracticeProject/Program.fs
@@ -0,0 +1,16 @@
+﻿let testList = [-10..+2..14]
+
+let tuplelist = [(2,3);(3,4);(0,2);(7,1)]
+
+let posList xs = List.map (fun x -> x > 0) xs
+
+let addTuples xs = List.map (fun (x,y) -> x+y) xs
+// test addTuples tuplelist;; 
+let isMember x ys = List.exists x ys
+
+// example test : isMember (fun x-> x=0) testList;;
+
+let disjoint (xs:list<int>) (ys:list<int>) = List.forall (fun x -> not (isMember x ys)) xs
+
+let oddList = [1..+2..9]
+let evenList = [0..+2..10]
