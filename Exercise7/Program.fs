d9b576b179ebeba66bbab8fbb04f65774715f14e
macastrogimenez <migca@itu.dk>
2025-08-22 08:23:29 +0200

    last exercise with continuation based function finished

--- Exercise7/Program.fs
+++ Exercise7/Program.fs
@@ -55,38 +55,65 @@ let rec sum (m,n) =
 
 // test: sum (4,1);; -> should be 9 -> PASSED
 // test: sum (5,1);; -> should be 11  -> PASSED
-// sum (5,250000);;
+// sum (5,200000);;
+// test: sum (5,2000000);;
 // test: sum (5,900000);;  -> crashes
 // test: sum (5,2000000);; -> crashes, memory exceeded
     
 let sumIt (m,n)=
-    if n > 0 
-    then List.fold (fun acc n -> acc + m+n) m [1.. +1 .. n]
+    if not (n = 0) then 
+        List.fold (fun acc n -> acc + m+n) m [1.. +1 .. n]
+    elif n < 0 then
+        raise (System.ArgumentException("n cannot be a negative number"))
     else m 
-    
+
 // test: sumIt (4,1);; -> should be 9  -> PASSED
 // test: sumIt (5,1);; -> should be 11  -> PASSED
+// test: sumIt (5,200000);;
 // test: sumIt (5,2000000);; -> integer overflow but worked-> PASSED
 // sumIt (5,200000000);;
 
-// Alternative functional iterative declaration using tail recursion with accumulator
-let sumAcc (m,n) =
-    let rec sumHelper i acc =
-        if i > n then acc
-        else sumHelper (i + 1) (acc + m + i)
-    
-    if n <= 0 then m
-    else sumHelper 0 0
 
-// test: sumAcc (4,1);; -> should be 9
-// test: sumAcc (5,1);; -> should be 11  
-// test: sumAcc (5,10);; -> should be 105
+let rec sumIt2 (m,n,acc) = 
+    if not (n = 0) then 
+        sumIt2 (m, n-1, acc+m+n)
+    elif n < 0 then
+        raise (System.ArgumentException("n cannot be a negative number"))
+    else acc + m 
+// test: sumIt2 (5,200000,0);;
+// test: sumIt2 (5,2000000,0);; -> integer overflow but worked-> PASSED
+// test: sumIt2 (5,200000000,0);; -> integer overflow but worked -> PASSED
+// test: sumIt2 (5,1,0);; -> should be 11 -> PASSED
+
 
 //------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
 // Exercise 7.3 HR exercise 9.4.
 // One iterative declaration is enough.
 (*Give iterative declarations of the list function List.length*)
+
+#time;;
+
+let rec listLength xs = 
+    let acc n f= n + f  
+    match xs with
+    |[]-> 0
+    |x::xs -> acc 1 (listLength xs)
+
+// test: listLength [1.. +1 .. 10000000];; -> failed, not tail recursive
+
+let rec listLength2 (accumulator, xs) = 
+    if not (xs = []) then listLength2(accumulator+1, xs.Tail) else accumulator
+
+// test: listLength2 (0,[1.. +1 .. 10000000]);; -> failed, not tail recursive
+
+let listLength3 xs = 
+    if not (xs = []) 
+        then List.fold (fun acc x -> acc + 1) 0 xs
+    else 0
+
+// test: listLength3 [1.. +1 .. 10000000];; -> passed, tail recursive
+
 
 //------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
@@ -115,6 +142,28 @@ Real: 00:00:00.012, CPU: 00:00:00.015,
 GC gen0: 0, gen1: 0,
 *)
 
+let rec fact n = 
+    match n with
+    | 0 -> 1
+    | n-> n*fact(n-1)
+
+#time;;
+// fact 35;;
+
+let rec fact2 a acc = 
+    if a > 0 then fact2 (a-1) (a*acc) else acc*1
+
+// fact2 35 1;;
+
+let rec fact3 a c = 
+    match a with
+    | 0 -> c 1
+    | n-> fact3 (n-1) (fun acc -> c (n*acc))
+
+let id a = a
+
+fact3 5 id;;
+
 //------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
 // Exercise 7.5 HR exercise 8.6.
@@ -133,6 +182,32 @@ correspond to the three cases of the above definition.
 Give an evaluations for F4.
 *)
 
+let rec fibW a = 
+    let mutable counter = 0
+    let mutable currentFibVal = 0
+    let mutable fibMinusOne = 1
+    let mutable fibMinusTwo= 0
+    while counter < a do
+        fibMinusTwo <- fibMinusOne 
+        fibMinusOne <- currentFibVal
+        if counter = 0 then 
+            currentFibVal <- 0
+            counter <- counter + 1
+            //printfn "when counter was %d,the current fib was %A" counter currentFibVal
+        elif counter = 1 then 
+            currentFibVal <- 1
+            counter <- counter + 1
+            //printfn "when counter was %d,the current fib was %A" counter currentFibVal
+        elif counter > 1 then 
+            currentFibVal <- fibMinusOne + fibMinusTwo
+            counter <- counter + 1
+            //printfn "when counter was %d,the current fib was %A" counter currentFibVal
+    currentFibVal
+// #time;;     
+// fibW 1;;
+// fibW 34;;
+
+
 //------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
 // Exercise 7.6 HR exercise 9.7.
@@ -146,3 +221,16 @@ definition of Fn given in Exercise 1.5.
 Compare these two functions using the directive #time, and compare this with the while-loop
 based solution of Exercise 8.6
 *)
+
+let rec fibA2 n n1 n2 =
+    if n = 0 then n2
+    elif n = 1 then n1
+    else fibA2 (n - 1) (n1 + n2) n1
+// fibA2 6 1 0;; -> PASSED
+
+let rec fibC n C =
+    if n = 0 then C 0
+    elif n = 1 then C 1
+    else fibC (n-1) ( fun res1 ->
+            (fibC (n-2) (fun res2 ->
+                C (res1 + res2))))
