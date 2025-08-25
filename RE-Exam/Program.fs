8763785a7867e5aa8e7f4d553a728ecaecf49517
macastrogimenez <migca@itu.dk>
2025-08-25 20:26:59 +0200

    The exam that could have been

--- RE-Exam/Program.fs
+++ RE-Exam/Program.fs
@@ -1,2 +1,49 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿let testList = [1.0 .. 10.0] 
+
+let f m fl = 
+    match fl with
+    |[] -> 0.00
+    |_ -> List.fold (fun acc x -> acc + x) 0 (List.map (fun x -> x*m) fl)
+//test: f 2 testList;; -> PASSED
+
+let f2 m fl = 
+    match fl with
+    |[] -> 0.00
+    |_ -> (List.fold (fun acc x -> acc + x) 0.00 fl) * m 
+// test: f2 2 testList;;
+
+let g fl = 
+    f 15.00 fl
+
+//test: g testList;; -> PASSED
+
+type concat<'a> = 
+    E
+    |C of 'a * concat<'a>
+
+let v = C('A',C('B',C('C',E)))
+
+let count c= 
+    let rec go c acc =
+        match c with 
+        |E -> acc
+        |C (a,b)-> go b (acc+1)
+    go c 0
+
+// test: count v;;
+
+let rec map f v = 
+    match v with 
+    |E -> E
+    |C (a,b)->  C (f a, map f b)
+    
+
+//test: map char.toLower v;;
+
+// map (fun x -> x.) v;; works
+
+let rec append v1 v2 = 
+    match v1 with
+    | E -> v2
+    | C (a,b) -> append b v2
+
